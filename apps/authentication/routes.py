cafba1e87d7d361b3156a1f6e745c987a7e513e6
iamjobiiiii <haritam.contact@gmail.com>
2024-06-13 15:05:51 +0530

    register route added

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -6,3 +6,7 @@ from apps.authentication import auth_bp
 @auth_bp.route('/login')
 def login():
     return render_template('/accounts/login.html')
+
+@auth_bp.route('/register')
+def register():
+    return render_template('/accounts/register.html')
