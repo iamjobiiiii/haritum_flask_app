38a855ed08de0e8c9252bdbeb00b58037eba498e
iamjobiiiii <haritam.contact@gmail.com>
2024-06-17 15:50:07 +0530

    routes.py edited

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,10 +1,8 @@
 from flask import render_template
-from home import main_bp
+from apps.home import main_bp
 
-@main_bp.route('/index')
-def index():
-    return render_template('home/index.html')
+
 
 @main_bp.route('/')
-def dashboard():
-    return render_template()
+def index():
+    return render_template('/home/index.html')
