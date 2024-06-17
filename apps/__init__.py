c784b0e17c3a5d4dc868e65ecb073572a7764342
iamjobiiiii <haritam.contact@gmail.com>
2024-06-17 15:53:57 +0530

    init edited

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,10 +3,10 @@
 
 from flask import Flask
 from apps.authentication.routes import auth_bp
-#from home.routes import main_bp
+from apps.home.routes import main_bp
 
 def create_app():
     app = Flask(__name__,)
-    #app.register_blueprint(main_bp)
+    app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     return app
